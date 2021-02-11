78d0fd1dc1a825253bfbbad10f0ecb16f2c1a6d7
no-bash <blunskay@gmail.com>
2021-02-11 17:45:36 +0300

    without adaptive

---
+++ js/animation.js
@@ -0,0 +1,11 @@
+VANTA.NET({
+    el: ".header",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 518.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x82abff,
+    backgroundColor: 0x1d13ac
+})
